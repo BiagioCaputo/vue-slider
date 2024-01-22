908a444b92a09e4af832286b392bb34802f0b78a
BiagioCaputo <scebiagio98@gmail.com>
2024-01-22 17:43:34 +0100

    creati cicli for con vue per le immagini e modifica nel css per sotituire il d-none con un v-show

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ console.log("Vue OK", Vue);
 const {createApp} = Vue;
 
 const app = createApp({
-    data(){
-    }
+    name:"caruselRielaborationWithVue",
+    data: () => ({
+        destinations, //recupero la lista oggetti contenuta in data.js
+        currentIndex: 0 , //imposto una variabile di appoggio che mi servirà per le operazioni di cambio immagine
+    }),
+    
 
 });
 
